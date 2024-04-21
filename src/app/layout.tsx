4103631a23f80fddbd30eda2f261a4bfd88f523b
Jason Wilson <info@jasonleewilson.com>
2024-04-20 21:06:22 -0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ItalianAffairPizza.com",
+  title: "FOR SALE | ItalianAffairPizza.com",
   description: "FOR SALE | ItalianAffairPizza.com",
 };
 
