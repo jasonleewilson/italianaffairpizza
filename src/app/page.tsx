99ae318f2dcd3ce164bfaea31468e41275c68cc9
Jason Wilson <info@jasonleewilson.com>
2024-05-01 14:47:42 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -207,7 +207,7 @@ export default function Home() {
           muted
           className='absolute z-10 w-auto min-w-full min-h-full max-w-none'
         >
-          <source src='/assets/video/bg-video-pizza-opt.m4v' type='video/mp4' />
+          <source src='/assets/video/bg-video-pizza-opt.mp4' type='video/mp4' />
           Your browser does not support the video tag.
         </video>
       </header>
